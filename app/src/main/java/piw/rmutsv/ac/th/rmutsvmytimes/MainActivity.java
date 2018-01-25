95ce83ad7261cb8580d4c8f66cda110f69a29652
Supawadee <supawadee018@gmail.com>
2018-01-25 11:43:02 +0700

    add commit success

--- app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
+++ app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/MainActivity.java
@@ -3,11 +3,24 @@ package piw.rmutsv.ac.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import piw.rmutsv.ac.th.rmutsvmytimes.fragment.Mainfragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+//        Add Fragment of Activity
+        if (savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment, new Mainfragment())
+                    .commit();
+
+        }
+
+
+    }//Main Method
 }
---
+++ app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/fragment/Mainfragment.java
@@ -0,0 +1,25 @@
+package piw.rmutsv.ac.th.rmutsvmytimes.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import piw.rmutsv.ac.th.rmutsvmytimes.R;
+
+/**
+ * Created by macbookpro on 1/25/2018 AD.
+ */
+
+public class Mainfragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.franment_main, container, false);
+        return view;
+    }
+
+}//Main Class
