2808cb8140802433ab2cc5439dd0524445f017bc
Supawadee <supawadee018@gmail.com>
2018-01-25 16:02:45 +0700

    Switch Controller LED Success

--- app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
+++ app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/fragment/ControllerFragment.java
@@ -51,6 +51,7 @@ public class ControllerFragment extends Fragment {
                 databaseReference.updateChildren(stringObjectMap);
 
             }//onClick
+            
         });
 
 //        On Controller
