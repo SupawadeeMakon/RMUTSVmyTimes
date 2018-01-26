8a1795c4c3baa2d750799f93c9dea473b0d842e7
Supawadee <supawadee018@gmail.com>
2018-01-26 09:51:07 +0700

    Add GraphFragment Success

---
+++ app/src/main/java/piw/rmutsv/ac/th/rmutsvmytimes/fragment/GraphFragment.java
@@ -0,0 +1,31 @@
+package piw.rmutsv.ac.th.rmutsvmytimes.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import piw.rmutsv.ac.th.rmutsvmytimes.R;
+
+/**
+ * Created by macbookpro on 1/26/2018 AD.
+ */
+
+public class GraphFragment extends Fragment {
+
+
+
+
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_graph, container, false);
+        return view;
+    }
+}//Main Class
